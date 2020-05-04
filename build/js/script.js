be586b5cb7ffac345f84b28155c667fde9392c9f
Flucky <fluckymendes@gmail.com>
2020-05-04 19:05:09 +0300

    Выполнено: Задание 3. Практика. Пишем приложение. Часть 1

--- build/js/script.js
+++ build/js/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 let money = +prompt('Ваш бюджет на месяц?', '');
-let time = prompt('Введите дату в формате YYYY-MM-DD');
+let time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
 let appData = {
   mounthlyBudget: money,
@@ -18,12 +18,35 @@ let appData = {
   savings: false
 };
 
-var expensesCategory = prompt('Введите обязательную статью расходов в этом месяце', '');
-let expensesAmount = prompt('Во сколько обойдется?', '');
-appData[expensesCategory] = expensesAmount;
+for (let i = 0; i < 2; i++) {
+  let expensesCategory = prompt('Введите обязательную статью расходов в этом месяце', '');
+  let expensesAmount = +prompt('Во сколько обойдется?', '');
+  if (expensesCategory !== null
+      && expensesCategory !== ``
+      && expensesAmount !== null
+      && expensesAmount !== ``) {
+    console.log(`done`);
+    appData.expenses[expensesCategory] = expensesAmount;
+  } else {
+    i--;
+    alert(`Значения не могут быть пустыми`);
+  }
+}
+
 console.log(appData);
 
-let dailyBudget = appData.mounthlyBudget / 30;
+appData.moneyPerDay = appData.mounthlyBudget / 30;
 
-alert('Ваш ежедневный бюджет: ' + dailyBudget);
+let budgetLevel;
+if (appData.mounthlyBudget < 10000) {
+  budgetLevel = `Маловато будет.`;
+} else if (appData.mounthlyBudget >= 10000 && appData.mounthlyBudget <= 30000) {
+  budgetLevel = `На хлеб с маслом хватит.`;
+} else if (appData.mounthlyBudget > 30000) {
+  budgetLevel = `Ну ты и аристократ!`;
+} else {
+  console.log(`Ошибка ввода`);
+}
+
+alert('Ваш ежедневный бюджет: ' + appData.moneyPerDay + ' рублей. ' + budgetLevel);
 
